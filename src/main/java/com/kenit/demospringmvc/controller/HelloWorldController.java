db0b9628cd4d2e5fdecaebdef45393f8a7d45e4e
- <=>
2022-06-25 17:06:17 +0700

    first commit

---
+++ src/main/java/com/kenit/demospringmvc/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.kenit.demospringmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home")
+public class HelloWorldController {
+
+    @GetMapping("")
+    public String helloWorld(){
+        return "index";
+    }
+}
---
+++ src/main/webapp/view/index.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Hello World</title>
+</head>
+<body>
+<h1 style="color: darkgreen">
+    Hello World
+</h1>
+</body>
+</html>
